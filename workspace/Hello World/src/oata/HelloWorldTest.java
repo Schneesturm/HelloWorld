7ef171284649b89356a4ca39d7b8b6f622d2fc6f
Torsten Otto <torsten.otto@itsrs.de>
2017-07-05 12:29:10 +0200

    Commit-Nachricht

---
+++ workspace/Hello World/src/oata/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package oata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	 public void testNothing() {
+	    }
+	    
+	 public void testWillAlwaysFail() {
+	        fail("An error message");
+	    }
+
+}
